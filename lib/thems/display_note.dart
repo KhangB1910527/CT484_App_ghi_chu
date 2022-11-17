85b159c4faeb743fdad345cdd7aed57d4f73eb7d
Khangb1910527 <khangb1910527@student.ctu.edu.vn>
2022-11-17 13:06:33 +0700

    project

---
+++ lib/thems/display_note.dart
@@ -0,0 +1,53 @@
+import 'package:appghichu/db/database_provider.dart';
+import 'package:appghichu/model/note_model.dart';
+import 'package:flutter/material.dart';
+
+class ShowNote extends StatefulWidget {
+  const ShowNote({Key key}) : super(key: key);
+
+  @override
+  State<ShowNote> createState() => _ShowNoteState();
+}
+
+class _ShowNoteState extends State<ShowNote> {
+  @override
+  Widget build(BuildContext context) {
+    final NoteModel note =
+        ModalRoute.of(context).settings.arguments as NoteModel;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Your Note"),
+
+        //delete
+
+        actions: [
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              DatabaseProvider.db.deleteNote(note.id);
+              Navigator.pushNamedAndRemoveUntil(context, '/', (route) => false);
+            },
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              note.title,
+              style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            Text(
+              note.body,
+              style: TextStyle(fontSize: 18.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/thems/thems.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppStyle {
+  static Color bgColor = Color.fromARGB(255, 227, 232, 236);
+  static Color mainColor = Color.fromARGB(255, 165, 170, 226);
+  static Color accentColor = Color.fromARGB(255, 42, 63, 217);
+
+  static List<Color> cardsColor = [
+    Colors.white,
+    Colors.red.shade100,
+    Colors.pink.shade100,
+    Colors.red.shade100,
+    Colors.orange.shade100,
+    Colors.yellow.shade100,
+    Colors.green.shade100,
+    Colors.blue.shade100,
+    Colors.blueGrey.shade100,
+  ];
+}
